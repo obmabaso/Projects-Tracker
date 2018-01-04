a246cf23ff554fe525e5ace2beba5148c6ad66f9
obmabaso <obmabaso@gmail.com>
2018-01-04 10:17:30 +0200

    Authentication cascading

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 col-md-offset-0">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    List of all projects
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ['middleware' => 'auth', function () {
+    return view('home');
+}]);
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
